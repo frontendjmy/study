d5e8be7dfc840b24e6c1a5c759d315a680f22ac1
4sgithubdef <frontendjmy@gmail.com>
2022-06-10 23:33:38 +0900

    changeBgBtn, changeBg 변수 추가

--- javascript/level-1/darkmode/app.js
+++ javascript/level-1/darkmode/app.js
@@ -1,17 +1,18 @@
 var count = 0;
 const changeBgBtn = document.querySelector(".change-btn");
+const changeBg = document.querySelector(".bg");
 
 function changeMode() {
   count++;
   if (count % 2 == 1) {
-    document.querySelector(".bg").classList.add("bg-dark");
-    document.querySelector(".change-btn").classList.add("btn-dark");
+    changeBg.classList.add("bg-dark");
+    changeBgBtn.classList.add("btn-dark");
     console.log("홀수");
   } else {
-    document.querySelector(".bg").classList.remove("bg-dark");
-    document.querySelector(".change-btn").classList.remove("btn-dark");
-    document.querySelector(".bg").classList.add("bg-white");
-    document.querySelector(".change-btn").classList.add("btn-white");
+    changeBg.classList.remove("bg-dark");
+    changeBgBtn.classList.remove("btn-dark");
+    changeBg.classList.add("bg-white");
+    changeBgBtn.classList.add("btn-white");
     console.log("짝수");
   }
 }
