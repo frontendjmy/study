dad1ee381af1ef4a8c4330efb65eec3e98a7f355
4sgithubdef <frontendjmy@gmail.com>
2022-06-13 15:49:08 +0900

    다음, 이전 버튼 추가

--- javascript/level-2/carousel/app.js
+++ javascript/level-2/carousel/app.js
@@ -1,8 +1,11 @@
-const firstBtn = document.querySelector(".btn1");
-const secondBtn = document.querySelector(".btn2");
-const thirdBtn = document.querySelector(".btn3");
+// btn 1,2,3
+const firstBtn = document.querySelector(".frist-btn");
+const secondBtn = document.querySelector(".second-btn");
+const thirdBtn = document.querySelector(".third-btn");
 
-function fristSlideEvent() {
+let photoNum = 1;
+
+function fristSlide() {
   document
     .querySelector(".slide-container")
     .classList.remove("slide-second-img");
@@ -11,17 +14,55 @@ function fristSlideEvent() {
     .classList.remove("slide-third-img");
 }
 
-function secondSlideEvent() {
+function secondSlide() {
   document.querySelector(".slide-container").classList.add("slide-second-img");
   document
     .querySelector(".slide-container")
     .classList.remove("slide-third-img");
+  photoNum = 2;
 }
 
-function thirdSlideEvent() {
+function thirdSlide() {
+  document.querySelector(".slide-container").classList.add("slide-second-img");
+  document;
   document.querySelector(".slide-container").classList.add("slide-third-img");
+  photoNum = 3;
 }
 
-firstBtn.addEventListener("click", fristSlideEvent);
-secondBtn.addEventListener("click", secondSlideEvent);
-thirdBtn.addEventListener("click", thirdSlideEvent);
+firstBtn.addEventListener("click", fristSlide);
+secondBtn.addEventListener("click", secondSlide);
+thirdBtn.addEventListener("click", thirdSlide);
+
+// 이전 다음
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+function next() {
+  photoNum += 1;
+  if (photoNum == 2) {
+    document
+      .querySelector(".slide-container")
+      .classList.add("slide-second-img");
+    photoNum == 2;
+  } else if (photoNum == 3) {
+    document.querySelector(".slide-container").classList.add("slide-third-img");
+    photoNum == 3;
+  }
+}
+
+function prev() {
+  if (photoNum >= 3) {
+    photoNum -= 1;
+    document
+      .querySelector(".slide-container")
+      .classList.remove("slide-third-img");
+  } else if (photoNum == 2) {
+    photoNum -= 1;
+    document
+      .querySelector(".slide-container")
+      .classList.remove("slide-second-img");
+  }
+}
+
+nextBtn.addEventListener("click", next);
+prevBtn.addEventListener("click", prev);
