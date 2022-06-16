b810971822614d2908895ae54fa03d72afac52e7
4sgithubdef <frontendjmy@gmail.com>
2022-06-16 19:17:58 +0900

    스크롤 이벤트

---
+++ javascript/level-2/scroll/tutorial/app.js
@@ -0,0 +1,39 @@
+// toggle
+const navBar = document.querySelector(".navbar-toggler");
+
+function show() {
+  document.querySelector(".list-group").classList.toggle("show");
+}
+
+navBar.addEventListener("click", show);
+
+// scroll event
+// font size change
+let scrollY = window.scrollY;
+
+function scrollEvent() {
+  if (window.scrollY > 200) {
+    document.querySelector(".navbar-brand").classList.add("fs-change");
+    console.log(window.scrollY);
+  } else {
+    document.querySelector(".navbar-brand").classList.remove("fs-change");
+  }
+}
+
+window.addEventListener("scroll", scrollEvent);
+
+// 회원약관 다 읽었을 시 alert
+
+const scrollTerms = document.querySelector(".terms");
+
+function scrollAlert() {
+  let termsTop = document.querySelector(".terms").scrollTop;
+  let termsHeight = document.querySelector(".terms").scrollHeight;
+  let termsDivHeight = document.querySelector(".terms").clientHeight;
+
+  if (termsTop + termsDivHeight == termsHeight) {
+    alert("alert");
+  }
+}
+
+scrollTerms.addEventListener("scroll", scrollAlert);
